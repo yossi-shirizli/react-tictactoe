2d2ccdd595cb4c3e4008ba182f5dbde3f9582694
Yossi Shirizli <yossi.shirizli@gmail.com>
2021-02-13 23:25:42 +0200

    created board and square components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Board from "./components/Board";
+
+const App = () => {
+  return (
+    <div>
+      <h1>TIC TAC TOE</h1>
+      <Board />
+    </div>
+  );
+};
+export default App;
