4bd66ba60f288e808e0f07ef3ebbb479f778cde8
Yossi Shirizli <yossi.shirizli@gmail.com>
2021-02-14 23:28:58 +0200

    added sass & styles

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,26 +1,26 @@
-import React from "react";
-import Square from "./Square";
+import React from 'react'
+import Square from './Square'
 
 const Board = () => {
-  return (
-    <div>
-      <div>
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+   return (
+      <div className="board">
+         <div className="board-row">
+            <Square value={0} />
+            <Square value={1} />
+            <Square value={2} />
+         </div>
+         <div className="board-row">
+            <Square value={3} />
+            <Square value={4} />
+            <Square value={5} />
+         </div>
+         <div className="board-row">
+            <Square value={6} />
+            <Square value={7} />
+            <Square value={8} />
+         </div>
       </div>
-      <div>
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
-      </div>
-      <div>
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
-      </div>
-    </div>
-  );
-};
+   )
+}
 
-export default Board;
+export default Board
